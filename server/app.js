ae23192fb099fd25a7be444aeb11e51a5d83f872
DESKTOP-F704TBT\User10299 <stanislavigov@gmail.com>
2025-02-22 15:06:58 +0200

    server responds with different html

--- server/app.js
+++ server/app.js
@@ -1,22 +1,28 @@
 const express = require('express')
 const app = express()
+const path = require('path');
 const port = 3000
 
+// app.get('/', (req, res) => {
+// 	// app.use(express.static('index.html'));
+// 	res.send('<h1>Home</h1>');
+
+// })
+
 app.get('/', (req, res) => {
-	res.send('<h1>Landing Page</h1>');
-})
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 app.get('/home', (req, res) => {
-  	res.send('<h1>Home</h1>');
+    res.sendFile(path.join(__dirname, 'home.html'));
 })
 
 app.get('/tasks', (req, res) => {
-  	res.send('<h1>Tasks</h1>');
+    res.sendFile(path.join(__dirname, 'tasks.html'));
 })
 
 app.get('/login', (req, res) => {
-
-	res.send("<h1>LOGIN</h1>");
+    res.sendFile(path.join(__dirname, 'login.html'));
 })
 
 app.listen(port, () => {
